88dcbbb8ab5b71df3b00563dac60cd11b6374cd2
feh-script <felyperock135@gmail.com>
2026-01-05 23:35:11 -0300

    coloquei gitignore e fiz implementacoes no codigoprincipal

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import http from 'http';
+import {v4} from 'uuid';
 const port = 3000;
+
 
 const grades = [{
     studentName: 'Alice',
@@ -10,7 +12,22 @@ const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/grades') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(grades));
-    }else {
+
+    }else if (req.method === 'POST' && req.url === '/grades') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const {studentName, subject, grade} = JSON.parse(body);
+            const newGrade = {id: v4(), studentName, subject, grade };
+            grades.push(newGrade);
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Grade added successfully' }));
+        });
+    } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Route not Found' }));
     }
